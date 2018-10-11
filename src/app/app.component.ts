1281b34007e922cdcb81eefceabb3e8222d647b7
fatters <mattallen27@gmail.com>
2018-10-11 13:11:34 +0100

    [#13] stop scroll to top component from hiding the footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, Renderer2 } from '@angular/core';
+
+const SCROLL_TRIGGER_POINT = 1000;
+const SCROLL_CLASS = 'has-scroll-component';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +10,17 @@ import { Component, HostListener } from '@angular/core';
 export class AppComponent {
   showScrollToTopComponent: boolean;
 
+  constructor(private renderer: Renderer2) {
+  }
+
   @HostListener('window:scroll', ['$event'])
   onWindowScroll() {
-    this.showScrollToTopComponent = window.scrollY > 1000;
+    if (window.scrollY > SCROLL_TRIGGER_POINT) {
+      this.showScrollToTopComponent = true;
+      this.renderer.addClass(document.body, SCROLL_CLASS);
+    } else {
+      this.showScrollToTopComponent = false;
+      this.renderer.removeClass(document.body, SCROLL_CLASS);
+    }
   }
 }
