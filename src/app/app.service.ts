0f7ef1ad750ec54cb09a030a0e73db7c50486f43
fatters <mattallen27@gmail.com>
2019-06-28 11:26:20 +0100

    use netlify env variables to confiure snoowrap

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,18 +3,18 @@ declare var require: any;
 export class AppService {
   private snoowrap: any;
 
-  constructor() {
+  constructor() {    
     const sw = require('snoowrap');
-
-    this.snoowrap = new sw({
-      userAgent: process.env.USER_AGENT,
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      refreshToken: process.env.REFRESH_TOKEN
-    });
+    this.snoowrap = new sw(this.configureSnooWrap());
   }
 
   getRepliesFromThread(threadId: string): Promise<any> {
     return this.snoowrap.getSubmission(threadId).expandReplies({limit: Infinity, depth: 0});
+  }
+
+  configureSnooWrap(): any {
+    return fetch(`/.netlify/functions/return-env`).then((data) => {
+      return data.json();
+    });
   }
 }
