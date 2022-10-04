1f927052ce21bdee5e11d17d39da7e44af8dd21a
fatters <mattallen27@protonmail.com>
2022-10-04 20:29:24 +0100

    clean up unused properties in environment objects

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  config: {}// require('../../config.json')
+  production: false
 };
 
 /*
