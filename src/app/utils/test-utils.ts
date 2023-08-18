8c0cbdb1f2970197b790cd27ab3a4f99e3ae8bcb
fatters <mattallen27@protonmail.com>
2023-08-18 16:35:18 +0100

    remove 'any' type

--- src/app/utils/test-utils.ts
+++ src/app/utils/test-utils.ts
@@ -2,11 +2,11 @@ import { DebugElement } from '@angular/core';
 import { ComponentFixture } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
-export const getNativeElement = (fixture: ComponentFixture<any>, cssClass: string): HTMLElement => {
+export const getNativeElement = (fixture: ComponentFixture<unknown>, cssClass: string): HTMLElement => {
   const element = fixture.debugElement.query(By.css(cssClass));
   return element ? element.nativeElement : null;
 };
 
-export const getDebugElements = (fixture: ComponentFixture<any>, cssClass: string): DebugElement[] => {
+export const getDebugElements = (fixture: ComponentFixture<unknown>, cssClass: string): DebugElement[] => {
   return fixture.debugElement.queryAll(By.css(cssClass));
 };
