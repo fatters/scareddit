492fe54b455c803aa76ad1516bd147f4a00947f5
fatters <mattallen27@protonmail.com>
2024-04-30 09:53:59 +0100

    add api directory for cloudflare

---
+++ functions/api/reddit-thread.js
@@ -0,0 +1,86 @@
+const snoowrapInit = require('snoowrap');
+const fs = require('fs');
+const path = require('path');
+
+let snoowrap;
+
+exports.handler = async (event, context) => {
+
+  const productionParam = event?.queryStringParameters?.production;
+  
+  const isProduction = (productionParam === 'true');
+
+  let localSnoowrapConfig = {};
+
+  if (!isProduction) {
+    try {
+      localSnoowrapConfig = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../snoowrap-config.json'), { encoding: 'utf8', flag: 'r' }));
+    } catch {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: `Error reading the local snoowrap-config.json` })
+      }
+    }
+  }
+
+  const {
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REFRESH_TOKEN,
+    USER_AGENT
+  } = isProduction ? process.env : localSnoowrapConfig;
+
+  const snoowrapConfig = {
+    userAgent: USER_AGENT,
+    clientId: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    refreshToken: REFRESH_TOKEN
+  }
+
+  const threadId = event?.queryStringParameters?.threadId;
+
+  const threadConfig = { // Not sure if we'll ever want to change these so static obj for now
+    limit: Infinity,
+    depth: 0
+  };
+
+  try {
+    if (!snoowrap) {
+      try {
+        snoowrap = new snoowrapInit(snoowrapConfig);
+      } catch (error) {
+        console.error(`Error initialising Snoowrap`, error);
+      }
+    }
+
+    if (snoowrap && threadId) {
+      try {
+        const thread = await snoowrap.getSubmission(threadId).expandReplies(threadConfig);
+        const id = thread.id ?? '';
+        const title = thread.title ?? 'Title not found'
+        const comments = thread.comments ?? [];
+
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ id, title, comments })
+        }
+      } catch {
+        return {
+          statusCode: 404,
+          body: JSON.stringify({ error: 'Reddit thread not found' })
+        }
+      }
+
+    } else {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: 'Bad Request' })
+      }
+    }
+  } catch {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed communicating with Snoowrap' })
+    }
+  }
+}
